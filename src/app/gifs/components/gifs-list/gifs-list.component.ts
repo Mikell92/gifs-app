87d9e33662d05d7fad7f19d65cdd129df4a8e523
Miguel Izquierdo <miguelizquierdo35@gmail.com>
2025-04-23 16:32:56 -0600

    Imagenes de relleno S. 6.74

---
+++ src/app/gifs/components/gifs-list/gifs-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
+
+@Component({
+  selector: 'gifs-list',
+  imports: [GifListItemComponent],
+  templateUrl: './gifs-list.component.html',
+})
+export class GifsListComponent { }
