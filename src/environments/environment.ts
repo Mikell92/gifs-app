7b699f397b43f5078a17dfd4573480ac79fea9fc
Miguel Izquierdo <miguelizquierdo35@gmail.com>
2025-04-24 16:28:25 -0600

    S 7.82

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,10 @@ export const environment = {
   companyName: 'Gifs',
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
+
   /* ApiKeys */
+  giphyApiKey: 'mpX0inlgiEWrYSF6ueNt3NYrlLwZOJhY',
+
   /* URLS */
+  giphyUrl: 'https://api.giphy.com/v1',
 };
